03abb32cd5e57570c27f20cc3301600de36c293b
makaszy <m.a.kaszynski@protonmail.com>
2024-01-13 21:17:27 +0100

    Made marquee util

--- src/js/landing-page/marquee.js
+++ src/js/landing-page/marquee.js
@@ -1,23 +1,13 @@
+import getNewAnimationDuration from "../utils/update-marquee-animation";
+
 
 function updateMarqueeAnimation() {
   const marquee = document.querySelector('.landing-page__marquee');
   const marqueeText = document.querySelector('.marquee-text');
-
-  const marqueeWidth = marquee.offsetWidth;
-  const textWidth = marqueeText.offsetWidth;
-
-  // Speed factor (pixels per second)
-  const speed = 70;
-
-  // Calculate the duration
-  const duration = (marqueeWidth + textWidth) / speed;
-
-  // Apply the duration to the animation
-  marqueeText.style.animationDuration = `${duration}s`;
+  marqueeText.style.animationDuration = getNewAnimationDuration(marquee, marqueeText);
 }
 
-updateMarqueeAnimation();
-// Run the function on load
+updateMarqueeAnimation(); // initial render
 
 window.addEventListener('resize', updateMarqueeAnimation);
 
---
+++ src/js/utils/update-marquee-animation.js
@@ -0,0 +1,16 @@
+function getNewAnimationDuration(marquee, marqueeText) {
+  const marqueeWidth = marquee.offsetWidth;
+  const textWidth = marqueeText.offsetWidth;
+
+  // Speed factor (pixels per second)
+  const speed = 70;
+
+  // Calculate the duration
+  const duration = (marqueeWidth + textWidth) / speed;
+
+  // Apply the duration to the animation
+  const newAnimationDuration = `${duration}s`
+  return newAnimationDuration;
+}
+
+export default getNewAnimationDuration;
