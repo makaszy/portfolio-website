337616674ed3f4d8f82cb32e07bcdba8051b1249
makaszy <m.a.kaszynski@protonmail.com>
2024-01-14 17:46:22 +0100

    Add animations to the projects list

--- src/js/projects-section/projects-section.js
+++ src/js/projects-section/projects-section.js
@@ -1,13 +1,29 @@
-import getNewAnimationDuration from "../utils/update-marquee-animation";
-
+import getNewAnimationDuration from '../utils/update-marquee-animation';
 
 function updateMarqueeAnimation() {
   const marquee = document.querySelector('.projects__marquee');
   const marqueeText = document.querySelector('.marquee-text--projects');
-  marqueeText.style.animationDuration = getNewAnimationDuration(marquee, marqueeText);
+  marqueeText.style.animationDuration = getNewAnimationDuration(
+    marquee,
+    marqueeText,
+  );
 }
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+const altHiddenElements = document.querySelectorAll('.hidden--alt');
+hiddenElements.forEach((el) => observer.observe(el));
+altHiddenElements.forEach((el) => observer.observe(el));
 
 updateMarqueeAnimation(); // initial render
 
 window.addEventListener('resize', updateMarqueeAnimation);
-
