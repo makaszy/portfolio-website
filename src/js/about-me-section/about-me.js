904e3da1c9bd60f61add085027d6f39a317fd047
makaszy <m.a.kaszynski@protonmail.com>
2024-01-16 15:32:48 +0100

    Create observer for sliding animation, and tech-list
    tech-list observes last item to override sidebar-btn color

--- src/js/about-me-section/about-me.js
+++ src/js/about-me-section/about-me.js
@@ -1,4 +1,5 @@
 import getNewAnimationDuration from '../utils/update-marquee-animation';
+import createObserver from '../utils/create-observer';
 
 function updateMarqueeAnimation() {
   const marquee = document.querySelector('.about__marquee');
@@ -9,5 +10,26 @@ function updateMarqueeAnimation() {
   );
 }
 updateMarqueeAnimation(); // initial render
-
 window.addEventListener('resize', updateMarqueeAnimation);
+
+// observer for sliding animation
+const observerAbout = createObserver('show');
+const hiddenElements = document.querySelectorAll('.hidden--tech-list');
+hiddenElements.forEach((el) => observerAbout.observe(el));
+
+// observe last item on the tech-list and overrides sidebar-btn color to fit the background
+
+const sidebarBtn = document.querySelector('.sidebar-btn--hamburger');
+const lastTechListItem = document.querySelector('.tech-list__li:last-child');
+
+const observerSidebarBtn = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      sidebarBtn.classList.add('.override-color--sidebar-btn');
+    } else {
+      sidebarBtn.classList.remove('.override-color--sidebar-btn');
+    }
+  });
+});
+
+observerSidebarBtn.observe(lastTechListItem);
