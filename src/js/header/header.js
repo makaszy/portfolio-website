41ffadda9a2900982fde29f189672ea62dfc00c6
makaszy <m.a.kaszynski@protonmail.com>
2024-01-20 14:08:51 +0100

    Fix issue with nav__links not closing header section

--- src/js/header/header.js
+++ src/js/header/header.js
@@ -5,3 +5,11 @@ hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('open');
   sidebar.classList.toggle('extend');
 });
+
+const navLinks = document.querySelectorAll('.nav__link');
+navLinks.forEach((navLink) => [
+  navLink.addEventListener('click', () => {
+    hamburger.classList.toggle('open');
+    sidebar.classList.toggle('extend');
+  }),
+]);
