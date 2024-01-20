941db34b1fd6fa97ebc57ef2dcd2b25639fbaae1
makaszy <m.a.kaszynski@protonmail.com>
2024-01-20 13:58:08 +0100

    Add animation to contact info

--- src/js/contact-section/contact-section.js
+++ src/js/contact-section/contact-section.js
@@ -1,25 +1,34 @@
 import getNewAnimationDuration from '../utils/update-marquee-animation';
+import createObserver from '../utils/create-observer';
 
 function updateContactMarqueeAnimation() {
   const marquee = document.querySelector('.contact__marquee');
   const marqueeText = document.querySelectorAll('.marquee-text--contact');
-  const marqueeTextAlt = document.querySelectorAll('.marquee-text--contact-alt');
+  const marqueeTextAlt = document.querySelectorAll(
+    '.marquee-text--contact-alt',
+  );
   marqueeText.forEach((marqueeTxt) => {
     // eslint-disable-next-line no-param-reassign
     marqueeTxt.style.animationDuration = getNewAnimationDuration(
       marquee,
-      marqueeTxt, 70
+      marqueeTxt,
+      70,
     );
-  })
+  });
 
   marqueeTextAlt.forEach((marqueeTxt) => {
     // eslint-disable-next-line no-param-reassign
     marqueeTxt.style.animationDuration = getNewAnimationDuration(
       marquee,
-      marqueeTxt, 120
+      marqueeTxt,
+      120,
     );
-  })
+  });
 }
 
 updateContactMarqueeAnimation();
 window.addEventListener('resize', updateContactMarqueeAnimation); // initial render
+
+const observerContact = createObserver('show--contact-info');
+const contactInfo = document.querySelector('.contact__info');
+observerContact.observe(contactInfo);
